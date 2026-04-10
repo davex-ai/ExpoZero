4f6afbc4ceb29de45c6d7a18b1cbbe4495460f20
davex-ai <omotoyedave4@gmail.com>
2026-04-10 03:53:17 +0100

    Modify TabOneScreen text and clean up styles
    Updated text content in TabOneScreen component and removed unused styles.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,22 +6,13 @@ import { Text, View } from 'react-native';
 export default function TabOneScreen() {
   return (
     <View className='flex-1 items-center justify-center bg-pink-500'>
-      <Text className='text-pink-900 text-2xl'>Tab One</Text>
-      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
+      <Text className='text-pink-900 text-2xl'>Hi This Is Expo Zero</Text>
+       <Text className='text-pink-900 text-2xl'>Thanks for using my setup</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
-  // title: {
-  //   fontSize: 20,
-  //   fontWeight: 'bold',
-  // },
   separator: {
     marginVertical: 30,
     height: 1,
